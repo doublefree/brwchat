d7e07a252d2538d195700379d0e50f8b6eab2af0
doublefree <takuya.watabe@gmail.com>
2011-06-26 10:42:19 +0900

    start server script is modified to debug

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-yaws --daemon --conf /Users/takuya/src/chat/conf/yaws.conf
+basedir=`dirname $0`
+debug=$1
+if test  "-d" = $debug; then
+    param="-i -T --conf $basedir/conf/yaws.conf"
+else
+    param="--daemon --conf $basedir/conf/yaws.conf"
+fi
+yaws $param
