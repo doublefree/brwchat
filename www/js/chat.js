c3ceb394c95242576a0f6a678538c7856ac4e650
doublefree <takuya.watabe@gmail.com>
2011-07-11 00:23:12 +0900

    message get handling is added to server and javascript

--- www/js/chat.js
+++ www/js/chat.js
@@ -28,15 +28,56 @@ $(function() {
     $('#get_message').submit(function() {
         $.get('chat_handler.yaws', 
             {
-                Message: 0
+                Message: chat_message.getMessageId()
             },
             success = function(data) {
-            alert(data);
-                // hide container
-                //$("#login_container").hide();
+                var messages = JSON.parse(data);
+                chat_message.showMessage(messages);
             }
         );
 
         return false;
     });
 });
+
+// chat message
+var Chat = {};
+Chat.Message = function() {
+    var messageId = 0;
+    var convertName = function(name) {
+        var convertedName = name;
+        switch (name) {
+            case "system":
+                convertedName = "システムメッセージ";
+                break;
+        }
+
+        return convertedName;
+    };
+    var escape = function(val) {
+        return $("<div/>").text(val).html();
+    }
+    return {
+        getMessageId : function() {
+            return messageId;
+        },
+        showMessage : function(messageList) {
+            for(var i = 0; i < messageList.length; i++) {
+                $("#chat_message").prepend(
+                    "<dt id=\"message" + messageList[i].log_id + "\">" +
+                        escape(messageList[i].name) +
+                    "</dt>" +
+                    "<dd>" + 
+                        "<div>" +
+                        escape(messageList[i].message) +
+                        "</div>" +
+                    "</dd>");
+                if(messageId < messageList[i].log_id) {
+                    messageId = messageList[i].log_id;
+                }
+            }
+        }
+    }
+};
+
+var chat_message = Chat.Message();
