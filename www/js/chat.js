7d1dff60a225bfc0cbfc8ce4eb9d1a345d14c6aa
doublefree <takuya.watabe@gmail.com>
2011-07-17 17:53:50 +0900

    change javascript style

--- www/js/chat.js
+++ www/js/chat.js
@@ -3,6 +3,68 @@
 * @author takuya-watabe
 */
 
+// chat message
+var Chat = {};
+Chat.Message = function() {
+    var messageId = 0;
+
+    var convertName = function(name) {
+        var convertedName = name;
+        switch (name) {
+            case "system":
+                convertedName = "システムメッセージ";
+                break;
+        }
+
+        return convertedName;
+    };
+
+    var escape = function(val) {
+        return $("<div/>").text(val).html();
+    }
+
+    var getMessageId = function() {
+        return messageId;
+    }
+
+    var getMessage = function(handler) {
+        $.get(handler, 
+            {
+                'messageId': getMessageId()
+            },
+            success = function(data) {
+                var messages = JSON.parse(data);
+                showMessage(messages);
+            }
+        );
+    }
+
+    var showMessage = function(messageList) {
+        for(var i = 0; i < messageList.length; i++) {
+            $("#chat_message").prepend(
+                "<dt id=\"message" + messageList[i].log_id + "\">" +
+                    escape(messageList[i].name) +
+                "</dt>" +
+                "<dd>" + 
+                    "<div>" +
+                    escape(messageList[i].message) +
+                    "</div>" +
+                "</dd>");
+            if(messageId < messageList[i].log_id) {
+                messageId = messageList[i].log_id;
+            }
+        }
+    }
+
+    return {
+        'getMessageId' : getMessageId,
+        'getMessage' : getMessage, 
+        'showMessage' : showMessage
+    }
+};
+
+var chat_message = Chat.Message();
+
 // onload
 $(function() {
     const chat_handler = 'chat_handler.yaws';
@@ -49,58 +111,3 @@ $(function() {
     });
 });
 
-// chat message
-var Chat = {};
-Chat.Message = function() {
-    var messageId = 0;
-    var convertName = function(name) {
-        var convertedName = name;
-        switch (name) {
-            case "system":
-                convertedName = "システムメッセージ";
-                break;
-        }
-
-        return convertedName;
-    };
-    var escape = function(val) {
-        return $("<div/>").text(val).html();
-    }
-    var getMessageId = function() {
-        return messageId;
-    }
-    var getMessage = function(handler) {
-        $.get(handler, 
-            {
-                'messageId': getMessageId()
-            },
-            success = function(data) {
-                var messages = JSON.parse(data);
-                showMessage(messages);
-            }
-        );
-    }
-    var showMessage = function(messageList) {
-        for(var i = 0; i < messageList.length; i++) {
-            $("#chat_message").prepend(
-                "<dt id=\"message" + messageList[i].log_id + "\">" +
-                    escape(messageList[i].name) +
-                "</dt>" +
-                "<dd>" + 
-                    "<div>" +
-                    escape(messageList[i].message) +
-                    "</div>" +
-                "</dd>");
-            if(messageId < messageList[i].log_id) {
-                messageId = messageList[i].log_id;
-            }
-        }
-    }
-    return {
-        'getMessageId' : getMessageId,
-        'getMessage' : getMessage, 
-        'showMessage' : showMessage
-    }
-};
-
-var chat_message = Chat.Message();
