332a5dca89d795c772e86c61de07c9bc7f2ed97e
doublefree <takuya.watabe@gmail.com>
2011-07-18 13:29:49 +0900

    sample script is moved from project root

--- sample/start_server.sh
+++ sample/start_server.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+# author TAKUYA watabe
+#
+# this sample startup script only tends to be executed at project root dir
+# (just upper sample dir)
+# ex  "./sample/start_server.sh"
 basedir=`dirname $0`
+
 debug=$1
 if test  "-d" = $debug; then
     param="-i -T --conf $basedir/conf/yaws.conf"
